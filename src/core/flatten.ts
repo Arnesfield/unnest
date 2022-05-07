4a8f41d3c42b1bde6c0aca210a15814c826cc470
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-05-07 15:02:20 +0800

    Update flatten() to allow merging cells

--- src/core/flatten.ts
+++ src/core/flatten.ts
@@ -39,6 +39,45 @@ export function flatToRows<T extends Record<string, any>>(
   });
 }
 
+function getAllKeys<T extends Record<string, any>>(items: T[]): (keyof T)[] {
+  const set = new Set<string>();
+  for (const item of items) {
+    for (const key in item) {
+      set.add(key);
+    }
+  }
+  return Array.from(set);
+}
+
+function getItemUsingKeys<T extends Record<string, any>>(
+  includeKeys: (keyof T)[] | undefined,
+  excludeKeys: (keyof T)[] | undefined,
+  ...items: T[]
+): T[] {
+  const allItems: T[] = [];
+  for (const item of items) {
+    let hasValue = false;
+    const obj: T = {} as T;
+    for (const key in item) {
+      const isIncluded = !includeKeys || includeKeys.includes(key);
+      const isExcluded = excludeKeys && excludeKeys.includes(key);
+      if (!isIncluded || isExcluded) {
+        continue;
+      }
+      const value = item[key];
+      const isValid = typeof value !== 'undefined' && value !== null;
+      if (isValid) {
+        obj[key] = value;
+        hasValue = true;
+      }
+    }
+    if (hasValue) {
+      allItems.push(obj);
+    }
+  }
+  return allItems;
+}
+
 /**
  * Flatten nested object to array of objects.
  * @param data The nested object to flatten.
@@ -59,7 +98,13 @@ export function flatten<
       return { [props.name]: item } as FlattenResult<T>;
     });
   }
-  const results2d = entries.map(([key, property]) => {
+
+  interface KeyedItems {
+    keys: K[];
+    results: FlattenResult<T>[];
+  }
+  let max = 0;
+  const keyedItems: KeyedItems[] = entries.map(([key, property]) => {
     const items = unwrap(data?.[key]);
     const results = ([] as FlattenResult<T>[]).concat(
       ...items.map(item => flatten<D, T>(item, property))
@@ -69,21 +114,72 @@ export function flatten<
       results.push({});
     }
     results[0][props.name as K] = data as T[K];
-    return results;
+    const keys = getAllKeys(results);
+    max = results.length > max ? results.length : max;
+    return { keys, results };
   });
-  // merge results2d
-  // get max length of results2d items
+
   const allResults: FlattenResult<T>[] = [];
-  const max = Math.max(...results2d.map(results => results.length));
-  for (let index = 0; index < max; index++) {
-    const items = results2d.reduce((items: FlattenResult<T>[], results) => {
-      const result = results[index];
-      result && items.push(result);
-      return items;
-    }, []);
-    // merge results
-    const result: FlattenResult<T> = Object.assign({}, ...items);
-    allResults.push(result);
+  const allConflictItems: FlattenResult<T>[] = [];
+
+  interface KeyedItem {
+    keys: K[];
+    result: FlattenResult<T>;
   }
+  for (let index = 0; index < max; index++) {
+    const propertyItemsToMerge: KeyedItem[] = [];
+    for (const { keys, results } of keyedItems) {
+      const result = results[index];
+      result && propertyItemsToMerge.push({ keys, result });
+    }
+    // get all duplicate keys
+    const keySet = new Set<K>();
+    const duplicateSet = new Set<K>();
+    for (const { keys } of propertyItemsToMerge) {
+      for (const key of keys) {
+        // skip current property
+        if (props.name !== key && keySet.has(key)) {
+          duplicateSet.add(key);
+        }
+        keySet.add(key);
+      }
+    }
+    // use first item as basis
+    const firstItem = propertyItemsToMerge.shift();
+    const finalResult: FlattenResult<T> = Object.assign(
+      {},
+      firstItem?.result || {}
+    );
+    const conflictKeys = Array.from(duplicateSet);
+    // merge
+    for (const { result } of propertyItemsToMerge) {
+      const validItems = getItemUsingKeys<FlattenResult<T>>(
+        undefined,
+        conflictKeys,
+        result
+      );
+      Object.assign(finalResult, ...validItems);
+      // check last conflict
+      const lastConflict = allConflictItems[allConflictItems.length - 1] as
+        | FlattenResult<T>
+        | undefined;
+      const conflictItems = getItemUsingKeys<FlattenResult<T>>(
+        conflictKeys,
+        undefined,
+        result
+      );
+      const conflictItemKeys = getAllKeys(conflictItems);
+      // new row or merge
+      if (!lastConflict || conflictItemKeys.some(key => key in lastConflict)) {
+        allConflictItems.push(...conflictItems);
+      } else {
+        Object.assign(lastConflict, ...conflictItems);
+      }
+    }
+    // save result
+    allResults.push(finalResult);
+  }
+  // merge conflict items
+  allResults.push(...allConflictItems);
   return allResults;
 }
